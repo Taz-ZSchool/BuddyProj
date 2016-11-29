1402996ebf9bb295dc38983b9a491626dd4c17d0
Taz-ZSchool <tanzim.zaman@carleton.ca>
2016-11-29 09:01:00 -0500

    Update BuddyInfo.java

--- src/BuddyInfo.java
+++ src/BuddyInfo.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -33,47 +32,4 @@ public class BuddyInfo {
 		}
 		return new BuddyInfo(info.get(0), info.get(1), info.get(2));
 	}
-
-
-}
-=======
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class BuddyInfo implements ActionListener {
-	
-	private String name;
-	private int age;
-	
-	public BuddyInfo(String name, int age) {
-			this.name = name;
-			this.age = age;
-		
-	}
-	
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	//Change in order to pull
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-
-}
->>>>>>> branch 'master' of https://github.com/Taz-ZSchool/BuddyProj.git
+)
